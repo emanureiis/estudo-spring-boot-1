8be12bf1333aff8e6b3d78e5b1074fd2086ee619
Emanuel Reis <emanuelreis8785@gmail.com>
2023-02-27 18:52:05 -0300

    Criando os primeiros Controllers

---
+++ api/src/main/java/clinica/java/api/controller/MedicoController.java
@@ -0,0 +1,24 @@
+package clinica.java.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+	
+	@PostMapping
+	public void cadastrar(@RequestBody String json) {
+		System.out.print(json);
+		msg();
+	}
+
+	private String msg() {
+		return "Cadastro realizado";
+		
+	}
+	
+
+}
