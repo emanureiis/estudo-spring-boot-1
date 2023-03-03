c30f1b61e2071047a912dddb52752b1c0d0b84fd
Emanuel Reis <emanuelreis8785@gmail.com>
2023-03-03 20:43:07 -0300

    Criando a classe MedicoRepository, responsável pela persistência dos dados do objeto

--- api/src/main/java/clinica/java/api/controller/MedicoController.java
+++ api/src/main/java/clinica/java/api/controller/MedicoController.java
@@ -1,5 +1,7 @@
 package clinica.java.api.controller;
 
+import clinica.java.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +12,13 @@ import clinica.java.api.medico.DadosCadastroMedico;
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
-	
+
+	@Autowired
+	private MedicoRepository repository;
+
+	//In here, the objetive is ask for the MedicoRepository persit our Object in the data base
 	@PostMapping
 	public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-		System.out.print(dados);
+		//repository.save();
 	}
 }
