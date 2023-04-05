39cb40b8f5271a9ea9f74625b841d0654434437b
Emanuel Reis <emanuelreis8785@gmail.com>
2023-04-04 22:39:42 -0300

    Projeto final do curso

--- api/src/main/java/med/voll/api/ApiApplication.java
+++ api/src/main/java/med/voll/api/ApiApplication.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:api/src/main/java/med/voll/api/ApiApplication.java
 package med.voll.api;
 
 import org.springframework.boot.SpringApplication;
@@ -12,18 +11,3 @@ public class ApiApplication {
 	}
 
 }
-=======
-package clinica.java.api;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class ApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-
-	}
-}
->>>>>>> 854877279d5db97ee5d891828063b87492ae55ba:api/src/main/java/clinica/java/api/ApiApplication.java
